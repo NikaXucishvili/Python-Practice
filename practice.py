7ce325dd704ac60537b8613881e199d570732d28
Niko <nikoloz.xucishvili655@gmail.com>
2024-07-08 23:28:00 +0400

    if else

--- practice.py
+++ practice.py
@@ -8,12 +8,31 @@ def my_function():
 my_function()
 
 
-name = input("Enter your name:\n")
-print("Your name is " + name)
+#name = input("Enter your name:\n")
+#print("Your name is " + name)
 
-age = input("Enter your age:\n")
-print("You are " + age + "years old")
-age2 = int(age) + 50
-print("In 50 years, you will be " + str(age2) + "years old") 
+#age = input("Enter your age:\n")
+#print("You are " + age + "years old")
+#age2 = int(age) + 50
+#print("In 50 years, you will be " + str(age2) + "years old " + "Probably Dead..") 
 
 
+
+#number = input("Please Enter a number between 1 and 10: ")
+
+#if int(number) <= 10:
+ #   print("A valid number was entered")
+#else:
+#    print("An invalid number was entered.")    
+#print("This will always print.")
+
+
+age = input("Please enter your age: ")
+if int(age) >= 50:
+	print("You are 50 or older.")
+elif int(age) >= 18:
+	print("You are an adult.")
+else:
+	print("You are not even an adult.")
+
+print("This will always print.")
