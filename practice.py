d778da2c79c45a8458621acc26a51ba0082c7596
Niko <nikoloz.xucishvili655@gmail.com>
2024-07-08 15:27:40 +0400

    function and input

--- practice.py
+++ practice.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-"""def my_function():
-	This is just some message
+def my_function():
+	"""This is just some message"""
 	global message
 	message = "Hello everyone"
 	print(message)
 my_function()
-print(message)"""
+print(message)
 
 print("Enter your name:")
 name = input()
